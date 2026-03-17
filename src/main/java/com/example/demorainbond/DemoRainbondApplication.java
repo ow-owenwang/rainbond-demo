48560d3c121b7dd4d15a71a5bd998bb69ef633cc
owenwang <ow.owenwang@gmail.com>
2026-03-17 14:48:46 +0800

    出实话

---
+++ src/main/java/com/example/demorainbond/DemoRainbondApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorainbond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRainbondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRainbondApplication.class, args);
+    }
+
+}
